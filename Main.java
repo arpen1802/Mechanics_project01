b50078564be37f9cd4f4b4f7aa00696fa0fff759
arpen1802 <matinyan.2000@gmail.com>
2020-03-02 22:06:23 +0400

    final version

--- Main.java
+++ Main.java
@@ -1,16 +1,50 @@
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car(10, 80, 2, 2);
-        Crossroad crossroad = new Crossroad(20, 4 );
-        System.out.println(checkStatus(car,crossroad));
+        //Problem 1
+        Crossroad crossroad = new Crossroad(20, 4);
+        for (double speed = 5.5; speed< 22.2; speed+=1.5){
+            Car car = new Car(speed, 50, 2, 2);
+            System.out.print("Initial speed is " + speed + ". ");
+            checkStatus(car,crossroad, false);
+        }
+
+        //Problem 2
+//        Car boundedSpeedCar = new Car(19, 50, 3, 2);
+//        checkStatus(boundedSpeedCar, crossroad, true);
+
+        //Problem 3
+//        Car carFront = new Car(40, 50, 1, 1);
+//        Car carBehind = new Car(40, 10, 3, 3);
+//        Crossroad crossroad1 = new Crossroad(10, 3 );
+//        checkFor2Cars(carFront, carBehind, crossroad1);
 
     }
-    public static int checkStatus(Car car, Crossroad crossroad){
+
+    private static double getPosition(Car car, Crossroad crossroad, boolean bounded){
         int t = crossroad.getLightTime();
-        double accPosition = car.getSpeed()*t + car.getPositiveAcc()*t*t/2;
+        double initialSpeed = car.getSpeed();
+        double accPosition = 0;
+        if(bounded){
+            for(int i = 1; i<=t; i++){
+                accPosition += initialSpeed;
+                if (initialSpeed+car.getPositiveAcc() < 27.7){
+                    initialSpeed +=car.getPositiveAcc();
+                }
+            }
+        }
+        else {
+            accPosition = car.getSpeed()*t + car.getPositiveAcc()*t*t/2;
+        }
+        return accPosition;
+    }
+
+    public static void checkStatus(Car car, Crossroad crossroad, boolean boundedSpeed){
+        int t = crossroad.getLightTime();
+        double accPosition = getPosition(car, crossroad, boundedSpeed);
         if (accPosition>=car.getDist()+crossroad.getWidth()){
-            return 1;
+            System.out.println("Drive!");
+            return;
         }
         int t0 = 1;
         double stopVelocity;
@@ -21,13 +55,45 @@ public class Main {
             }
             t0+=1;
         }
-        double stopPosition = car.getSpeed()*t + car.getNegativeAcc()*t0*t0/2;
-        System.out.println(accPosition);
-        System.out.println(stopVelocity);
+        double stopPosition = car.getSpeed()*t0 + car.getNegativeAcc()*t0*t0/2;
+
         if(stopPosition <= car.getDist()){
-            return 2;
+            System.out.println("Stop!");
         }
-        System.out.println("Start stopping " + (t0-t) + " seconds earlier.");
-        return 0;
+        else {
+            System.out.println("Start stopping " + ((t0-t)>t0?t0:(t0-t)) + " seconds earlier.");
+        }
+    }
+
+
+    public static void checkFor2Cars(Car car1, Car car2, Crossroad crossroad){
+        int t = crossroad.getLightTime();
+        double accPosition1 = car1.getSpeed()*t + car1.getPositiveAcc()*t*t/2;
+        double accPosition2 = car2.getSpeed()*t + car2.getPositiveAcc()*t*t/2;
+        if (accPosition1>=car1.getDist()+crossroad.getWidth()){
+            System.out.println("Car1 Drive!");
+        }
+        else {
+            System.out.println("Car1 Stop!");
+        }
+        if(accPosition2<accPosition1+car1.getDist() && accPosition2 >= car1.getDist()+car2.getDist()+crossroad.getWidth()){
+            System.out.println("Car2 Drive!");
+            return;
+        }
+
+        double relativeAcceleration = car1.getPositiveAcc()-car2.getPositiveAcc();
+        double relativeVelocity = car1.getSpeed() - car2.getSpeed() + t*(relativeAcceleration);
+
+        int t0 = 1;
+        double relativePos;
+
+        while (t0<100){
+            relativePos = -relativeVelocity * t0 + relativeAcceleration *t0*t0/2;
+            if(relativePos>-crossroad.getWidth()){
+                System.out.println("Car2 Stop!");
+                return;
+            }
+            t0+=1;
+        }
     }
 }
